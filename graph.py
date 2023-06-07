a483aefd3dcedfa05103b56580619c83b3d88c81
Lydia Csaszar <applelid07@gmail.com>
2023-06-07 15:28:08 -0400

    working graph data output

--- graph.py
+++ graph.py
@@ -2,50 +2,28 @@
 '''
 An attempt to create a structure and class for the graphs 
 Inspired by the class tutorials at https://stackoverflow.com/questions/40898482/defining-method-of-class-in-if-statement
-
 '''
+
 import os
 import math as m
-import wire as w
-
-
-class u:
-    def __init__(self, affix_type):
-        self.affix_type = affix_type
-     
-    #For prefix 
-    count = []
-    terminal = []
-    wire = []
-
-    #For suffix
-    count2 = []
-    terminal2 = []
-    wire2 = []
-    
-    @staticmethod 
-    def prefix_info(count, terminal, wire):
-        u.count.append(count)
-        u.terminal.append(terminal)
-        u.wire.append(wire)
-                        
-    @staticmethod
-    def suffix_info(count2, terminal2, wire2):
-        u.count2.append(count2)
-        u.terminal2.append(terminal2)
-        u.wire2.append(wire2)
-        
-    #Set the affix type
-    def affix(self):
-        if self.affix_type == 'prefix':
-            self.affix = u.prefix_info(count, terminal, wire)
-        else:
-            self.affix = u.suffix_info(count, terminal, wire)
+import wire as w 
             
 def graph_maker(kmer_list):
+
+    coverage = 100 #assuming 100 for the fake_dna sample that chatGPT created
+    dict_str = () #takes a string from a dictonary value
+
+    #A bunch of value initializations.
+    prefix_dict = {}
+    suffix_dict = {}
+    found_countA = 0
+    found_countB = 0
+    vertex_count = 0
+    vertex_count2= 0    
+    
     enum = enumerate(kmer_list) #count the number of strings in the list to prep it for dictionary conversion
 
-    kmer_dict = dict((x,y) for x,y in enum)
+    kmer_dict = dict((x,y) for x,y in enum) #creating dictionary
 
     #send to a dictonary instead of a tuple because a tuple is immutable. 
     lmer1 = [] 
@@ -69,86 +47,130 @@ def graph_maker(kmer_list):
             else: 
                 count -= 3
                 flag=False
-        
+
         lmerA = ''
         lmerB = ''
-        for x in range(0,len(lmer1)):   #In theory these should be the same length of k_size
+        for x in range(0,len(lmer1)): #In theory these should be the same length of k_size
             lmerA+=lmer1[x] 
             lmerB+=lmer2[x]
- 
+        
         lmer1.clear()
         lmer2.clear()
         
-        dict_str = () #takes a string from a dictonary value
-        alpha_size = 0
-        vertex_count = 0
-        coverage = 100 #assuming 100 for the fake_dna sample that chatGPT created
-        prefix_dict = {}
-        suffix_dict = {}
-        alpha_size2 =0
-        vertex_count2=0
-        found_countA = 0
-        found_countB = 0
-        
-        label = u('prefix') 
+        label = u('prefix')#Initialize the data structure for the prefix type in the class 
         for d in range(len(kmer_dict)-1): #FIND EDGES FOR PREFIX
-            count1 = 0
             for a in alpha:
-                alpha_size +=1
                 dict_str = kmer_dict.get(d)
                 temp_lmer = a+lmerA
-              
-                if(dict_str == temp_lmer):
-                    #print("FOR LETTER:", a) 
-                    #print("DICTONARY VALUE", kmer_dict.get(d)) 
-                    #print("ADDED LETTER:  ", temp_lmer) 
-                    count1 += 1
-                    vertex_count = m.ceil(count1/coverage)
-                    #print("Vertex Count (vc):", vertex_count)
-                    #print("In Dict\n")
-                    prefix_dict.update({temp_lmer:vertex_count})
-                    label.prefix_info(list(prefix_dict), [0], [0])
-                    found_countA += 1     
-                else:
-                    pass 
+                comp = dict_str == temp_lmer
 
-        lmerA=''           
-        label2 = u('suffix')
+                #If the string exists in the dictionary
+                if(comp == True):
+                    found_countA += 1
+                    #Gives us the number of vertices that the string should have
+                    vertex_count = m.ceil(found_countA/coverage)
+                    app = {temp_lmer:vertex_count}
+                    prefix_dict.update(app)
+                    logic = vertex_count==1
+                    temp=[]
+                    temp = app.copy()
+            
+                    if(logic is True): #Marks a terminal node if edge is 1
+                        label.prefix_info(temp, 1, 0)    
+                    else:
+                        label.prefix_info(temp, 0, 0)
+        lmerA=''
+                 
+        label2 = u('suffix') #Initialize the data structure for the suffix type in the class
         for d2 in range(len(kmer_dict)-1): #FIND EDGES FOR SUFFIX
-            count2 = 0
             for a2 in alpha:
-                alpha_size2 +=1
                 dict_str2 = kmer_dict.get(d2)
                 temp_lmer2 = lmerB+a2
                 if(dict_str2 == temp_lmer2):
-                    #print("FOR LETTER:", a2) 
-                    #print("DICTONARY VALUE", kmer_dict.get(d2)) 
-                    #print("ADDED LETTER:  ", temp_lmer2)
-                    count2 += 1
-                    vertex_count2 = m.ceil(count2/coverage)
-                    #print("Vertex Count (vc):", vertex_count)
-                    #print("In Dict\n")
-                    suffix_dict.update({temp_lmer2:vertex_count2})
-                    label2.suffix_info(list(suffix_dict), [0], [0])
                     found_countB += 1
-                    
-                else:
-                    pass
+                    #Gives us the number of vertices that the string should have
+                    vertex_count2 = m.ceil(found_countB/coverage)          
+                    app2 = {temp_lmer2:vertex_count2}
+                    suffix_dict.update(app2)
+                    logic2 = vertex_count2==1
+                    temp2=[]
+                    temp2 = app2.copy()
+            
+                    if(logic2 is True): #Marks a terminal node if edge is 1
+                        label2.suffix_info(temp2, 1, 0)
+                    else:
+                        label2.suffix_info(temp2, 0, 0)             
         lmerB=''  
-         
-    ulength = len(u.count)  
-    for h in range(ulength):   
-        print(u.count[h],u.terminal[h])
-        print(u.count2[h],u.terminal[h])
-    
+        
+    #appends the values into suffixes since its hard to do so in a python class    
+    ulength = len(u.count) 
+    for h in range(ulength):  
+        val = ''
+        val2 = ''
+        val =  u.count[h],u.terminal[h],u.wire[h]
+        val2 = u.count2[h],u.terminal2[h],u.wire2[h]
+        u.prefixes.append(val)
+        u.suffixes.append(val2)
+        
+    #save data to files for each affix type
+    curdir = os.path.dirname(__file__)
+    file_path1 = curdir+'/fasta_files/gd_prefix.txt'
+    file_path2 = curdir+'/fasta_files/gd_suffix.txt' 
+    fd = open(file_path1, "w")
+    fd.write("_______PREFIXES__________\n")
+    fd.write(str(u.prefixes))
+    fd.close()
+    fd = open(file_path2, "w")
+    fd.write("_______SUFFIXES__________\n") 
+    fd.write(str(u.suffixes))
+    fd.close()
+    print("DONE")
 '''
         LEFT TO DO:
 			wire_info = wire(u)
 			graph = u.wire_info.info()
     return graph
 '''
+
+class u:
+    def __init__(self, affix_type):
+        self.affix_type = affix_type
+    
+    #affixes    
+    prefixes = []
+    suffixes = []
+    
+    #For prefix 
+    count = []
+    terminal = []
+    wire = []
+
+    #For suffix
+    count2 = []
+    terminal2 = []
+    wire2 = []
+   
+    @staticmethod 
+    def prefix_info(count, terminal, wire):
+        u.count.append(count)
+        u.terminal.append(terminal)
+        u.wire.append(wire)
+                        
+    @staticmethod
+    def suffix_info(count2, terminal2, wire2):
+        u.count2.append(count2)
+        u.terminal2.append(terminal2)
+        u.wire2.append(wire2)
+        
+    #Set the affix type
+    def affix(self):
+        if self.affix_type == 'prefix':
+            self.affix = u.prefix_info()
+        else:
+            self.affix = u.suffix_info()
+
  
-####CALLING THE FUNCTION####
+'''    CALLING THE FUNCTION    '''
 curdir = os.path.dirname(__file__)
 path = os.path.join(curdir+"/fasta_files/dna_kmer.txt")
 fd = open(path, 'r')
